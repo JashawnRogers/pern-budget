89aad3861414e5a4f874663686c53648644abc25
JashawnRogers <rogersjashawn20@gmail.com>
2025-03-17 09:58:01 -0700

    Created route for updating transactions

--- server/controller/transactionsController.js
+++ server/controller/transactionsController.js
@@ -20,7 +20,7 @@ module.exports = {
             await client.query('BEGIN')
 
             const insertQuery = 'INSERT INTO transactions (user_id, amount, type, category, description, vendor) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;'
-            const { rows } = await pool.query(insertQuery, [userId, amount, type, category, description || null, vendor || null])
+            const { rows } = await client.query(insertQuery, [userId, amount, type, category, description || null, vendor || null])
 
             await client.query('COMMIT')
             res.status(201).json({ message: 'Transaction added successfully', transaction: rows[0] })
@@ -32,4 +32,62 @@ module.exports = {
             client.release() // release client back to pool
         }
     },
+    updateTransaction: async (req, res) => {
+        const client = await pool.connect()
+
+        try {
+            if (!req.session.user) {
+                return res.status(401).json({ message: 'User not authorized' })
+            }
+
+            const userId = req.session.user.id
+            const transactionId = req.params.id
+
+            const { amount, type, category, description, vendor } = req.body
+
+            if (amount === undefined && type === undefined && category === undefined && description === undefined && vendor === undefined) {
+                return res.status(400).json({ error: 'No valid fields provided for update' })
+            }
+
+            // COALESCE updates only the fields that are provided. If the field is not provided, the original value continues to persist in db
+            const updateQuery = `
+            UPDATE transactions 
+            SET 
+                amount = COALESCE($1, amount), 
+                type = COALESCE($2, type), 
+                category = COALESCE($3, category), 
+                description = COALESCE($4, description), 
+                vendor = COALESCE($5, vendor)
+            WHERE id = $6 AND user_id = $7 
+            RETURNING *, user_id;
+            `
+            // If amount is undefined, null is used and COALESCE will keep the existing value
+            const queryValues = [
+                amount || null,
+                type || null,
+                category || null,
+                description || null,
+                vendor || null,
+                transactionId,
+                userId
+            ]
+
+
+            await client.query('BEGIN')
+            const { rows } = await client.query(updateQuery, queryValues)
+            await client.query('COMMIT')
+
+            if (rows.length === 0) {
+                return res.status(404).json({ message: 'Transaction not found or user not authorized' })
+            }
+
+            res.status(200).json({ message: 'Transaction successfully updated', transaction: rows[0] })
+        } catch (error) {
+            await client.query('ROLLBACK')
+            console.error(error)
+            return res.status(500).json({ message: 'Internal server error', error: error.message })
+        } finally {
+            client.release()
+        }
+    }
 }
--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const transactionsController = require('../controller/transactionsController')
 
 router.post('/create-transaction', transactionsController.createTransaction)
+router.put('/update-transaction/:id', transactionsController.updateTransaction)
 
 module.exports = router
