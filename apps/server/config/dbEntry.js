c3b168a13f35298a4a7d1813750cf4e4f938b2aa
JashawnRogers <rogersjashawn20@gmail.com>
2025-05-21 06:56:41 -0700

    debugging pg connection

--- apps/server/config/dbEntry.js
+++ apps/server/config/dbEntry.js
@@ -4,7 +4,11 @@ require('dotenv').config()
 //Parse NUMERIC as float instead of string
 types.setTypeParser(types.builtins.NUMERIC, (value) => parseFloat(value))
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
     ssl: {
         rejectUnauthorized: false // Required for Supabase SSL
     }
